9810012c0f139acf4a2b071a1dcfdc7ecfee3777
Alan Velasco <AlanVelasco.A@gmail.com>
2018-05-06 17:53:11 -0500

    Return jsons correctly

--- inscript/views.py
+++ inscript/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
@@ -13,13 +14,13 @@ def index(request):
 def teacher_courses(request, teacher_name):
     courses = Course.objects.filter(teacher__name=teacher_name)
     context = {"courses": courses}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 
 def subject_courses(request, subject_name):
     courses = Course.objects.filter(subject__name=subject_name)
     context = {"courses": courses}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 
 def available_classrooms(request, time_slot):
@@ -28,14 +29,14 @@ def available_classrooms(request, time_slot):
     not_available = [course.classroom.number for course in courses_given]
     available = classrooms.exclude(number__in=not_available)
     context = {"classrooms": available}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 
 def busy_teachers(request, time_slot):
     courses = Course.objects.filter(time_slot=time_slot)
     busy_teachers = [course.teacher for course in courses]
     context = {"teachers": busy_teachers}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 
 def available_teachers(request, time_slot):
@@ -43,7 +44,7 @@ def available_teachers(request, time_slot):
     busy_teachers = [course.teacher.employee_id for course in courses]
     available = Teacher.objects.exclude(employee_id__in=busy_teachers)
     context = {"teachers": available}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
 
 
 def which_course(request, day, classroom):
@@ -51,4 +52,4 @@ def which_course(request, day, classroom):
         time_slot__contains=day, classroom__number=classroom
     )
     context = {"courses": courses}
-    return context
+    return HttpResponse(json.dumps(context), content_type='application/json')
