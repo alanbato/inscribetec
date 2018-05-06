0399dbbf31b47b9fca15cad5ee52c446ba4cd136
Alan Velasco <AlanVelasco.A@gmail.com>
2018-05-05 20:29:34 -0500

    Updatey

--- inscribe/settings.py
+++ inscribe/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(7^!tdozl=!i*-%-qkh=u9e3s#327l7+oo%3!)aj18pyhreyg#'
+SECRET_KEY = "(7^!tdozl=!i*-%-qkh=u9e3s#327l7+oo%3!)aj18pyhreyg#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,52 +31,54 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "inscript.apps.InscriptConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'inscribe.urls'
+ROOT_URLCONF = "inscribe.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'inscribe.wsgi.application'
+WSGI_APPLICATION = "inscribe.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_URL', 'db.pgsql')
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DATABASE_URL", "inscribetec"),
     }
 }
 
@@ -86,26 +88,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'America/Monterrey'
+TIME_ZONE = "America/Monterrey"
 
 USE_I18N = True
 
@@ -113,8 +109,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
-STATIC_URL = '/static/'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"),)
--- inscript/models.py
+++ inscript/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from enum import Enum
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+
+class Department(Enum):
+    CS = "Computer Science"
+    ESCOLAR = "Escolar"
+    OTRO = "Otro Dpto."
+
+
+class Teacher(models.Model):
+    employee_id = models.CharField(max_length=9, unique=True)
+    name = models.CharField(max_length=25)
+    telephone = PhoneNumberField(blank=True)
+    email = models.EmailField()
+    num_classes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Classroom(models.Model):
+    number = models.CharField(max_length=10, unique=True)
+    capacity = models.IntegerField(default=1)
+    administrator = models.CharField(
+        max_length=10, choices=[(tag.name, tag.value) for tag in Department]
+    )
+
+    def __str__(self):
+        return self.number
+
+
+class Subject(models.Model):
+    key = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=25)
+    lab_hours = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+    classroom = models.ForeignKey(Classroom, on_delete=models.PROTECT)
+    teacher = models.ManyToManyField(Teacher)
+    group_num = models.IntegerField(default=1)
+    time_slot = models.TextField()
+    lab_slot = models.TextField()
+    responsability = models.IntegerField(default=100)
+    in_english = models.BooleanField(default=False)
+    is_honors = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "{subject} - Gpo {group}".format(
+            subject=self.subject, group=self.group_num
+        )
